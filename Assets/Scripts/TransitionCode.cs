71cb67f495a42c9b254aed088113d3bfe26a465e
ishaniKapoor <48035591+ishaniKapoor@users.noreply.github.com>
2020-02-25 20:47:20 -0500

    TransitioningBasic

---
+++ Assets/Scripts/TransitionCode.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransitionCode : MonoBehaviour
+{
+    private GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision obj)
+    {
+        if (obj.GameObject.tag == "Player")
+        {
+            //do something with changing the map
+        }
+    }
+
+}
