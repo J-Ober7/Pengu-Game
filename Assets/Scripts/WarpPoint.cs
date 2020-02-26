b29263090fa8e5e6d863d6fa2af9ca8e55f514e5
JoberXeven <45186143+JoberXeven@users.noreply.github.com>
2020-02-25 21:16:39 -0500

    WarpPOoints Starter

---
+++ Assets/Scripts/WarpPoint.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarpPoint : MonoBehaviour
+{
+
+    public Transform destination;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public Vector2 getDestination() {
+
+        return destination.position;
+    }
+}
